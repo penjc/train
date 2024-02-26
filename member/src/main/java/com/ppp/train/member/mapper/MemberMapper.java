3986cfb432514432644dbadf9c4eee1627027bfc
peng <2686728826@qq.com>
2024-02-27 03:17:31 +0800

    the official mybatis code generator

--- member/src/main/java/com/ppp/train/member/mapper/MemberMapper.java
+++ member/src/main/java/com/ppp/train/member/mapper/MemberMapper.java
@@ -1,5 +1,31 @@
 package com.ppp.train.member.mapper;
 
+import com.ppp.train.member.domain.Member;
+import com.ppp.train.member.domain.MemberExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface MemberMapper {
-    Integer count();
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
 }
