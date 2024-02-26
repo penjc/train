4d2a149c49c3905730c6faac175c367aa7b6d757
peng <2686728826@qq.com>
2024-02-27 03:33:48 +0800

    easy register interface

--- member/src/main/java/com/ppp/train/member/service/MemberService.java
+++ member/src/main/java/com/ppp/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.ppp.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.ppp.train.member.domain.Member;
+import com.ppp.train.member.domain.MemberExample;
 import com.ppp.train.member.mapper.MemberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +17,22 @@ public class MemberService {
 
     public Integer count(){
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if(CollUtil.isEmpty(list)) {
+            Member member = new Member();
+            member.setId(System.currentTimeMillis());
+            member.setMobile(mobile);
+            memberMapper.insert(member);
+            return member.getId();
+        }
+        else{
+//            return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
     }
 }
