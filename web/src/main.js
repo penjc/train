839ffa545224c719c249fe278dffbf8cc88c4da0
peng <2686728826@qq.com>
2024-02-27 21:25:07 +0800

    增加login路由和页面

--- web/src/main.js
+++ web/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/reset.css';
+import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
 
 const app = createApp(App);
