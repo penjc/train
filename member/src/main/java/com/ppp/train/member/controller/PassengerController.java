18f0765f9b7a9dc934b6674a728a2d894c4577d2
peng <2686728826@qq.com>
2024-02-29 01:34:25 +0800

    乘车人列表查询接口，查看当前会员自己添加的乘客

--- member/src/main/java/com/ppp/train/member/controller/PassengerController.java
+++ member/src/main/java/com/ppp/train/member/controller/PassengerController.java
@@ -1,14 +1,16 @@
 package com.ppp.train.member.controller;
 
+import com.ppp.train.common.context.LoginMemberContext;
 import com.ppp.train.common.resp.CommonResp;
+import com.ppp.train.member.req.PassengerQueryReq;
 import com.ppp.train.member.req.PassengerSaveReq;
+import com.ppp.train.member.resp.PassengerQueryResp;
 import com.ppp.train.member.service.PassengerService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -23,4 +25,10 @@ public class PassengerController {
         return new CommonResp<>();
     }
 
+    @GetMapping("/query-list")
+    public CommonResp<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req){
+        req.setMemberId(LoginMemberContext.getMemberId());
+        List<PassengerQueryResp> passengerQueryRespList = passengerService.queryList(req);
+        return new CommonResp<>(passengerQueryRespList);
+    }
 }
--- member/src/main/java/com/ppp/train/member/service/PassengerService.java
+++ member/src/main/java/com/ppp/train/member/service/PassengerService.java
@@ -2,13 +2,19 @@ package com.ppp.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import com.ppp.train.common.context.LoginMemberContext;
 import com.ppp.train.common.util.SnowUtil;
 import com.ppp.train.member.domain.Passenger;
+import com.ppp.train.member.domain.PassengerExample;
 import com.ppp.train.member.mapper.PassengerMapper;
+import com.ppp.train.member.req.PassengerQueryReq;
 import com.ppp.train.member.req.PassengerSaveReq;
+import com.ppp.train.member.resp.PassengerQueryResp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerService {
@@ -23,5 +29,16 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
+    }
+
+    public List<PassengerQueryResp> queryList(PassengerQueryReq passengerQueryReq){
+        PassengerExample passengerExample = new PassengerExample();
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        if(ObjectUtil.isNotNull(passengerQueryReq.getMemberId())) {
+            criteria.andMemberIdEqualTo(passengerQueryReq.getMemberId());
+        }
+        List<Passenger> passengersList = passengerMapper.selectByExample(passengerExample);
+        List<PassengerQueryResp> list = BeanUtil.copyToList(passengersList, PassengerQueryResp.class);
+        return list;
     }
 }
